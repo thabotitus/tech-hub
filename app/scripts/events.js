f841cecf4502f8c9f1f9ec2b12439c1b67a2b5f5
Thabo Titus <hello@thabotitus.co.za>
2017-05-11 11:00:46 +0200

    loop through stuff on page

--- app/scripts/events.js
+++ app/scripts/events.js
@@ -1,11 +1,11 @@
 var events = new Vue({
   el: '#events',
   data: {
-    message: 'This is a message'
+    events: 'this.foo()'
   },
   methods: {
     foo: function(){
-      return 'I am a function';
+      return fetch_from_github();
     }
   }
 })
@@ -13,7 +13,7 @@ var events = new Vue({
 function fetch_from_github() {
   return [ { type: 'push',
              handle: 'Billy Bob',
-             image_url: 'http://foo',
+             image_url: 'https://s-media-cache-ak0.pinimg.com/736x/3e/de/0b/3ede0bf72b5e51dc895714f1f6d9ee7d.jpg',
              repo: 'prodigy',
              branch: 'master',
              date: '2017-03-22',
@@ -21,7 +21,7 @@ function fetch_from_github() {
            },
            { type: 'create_branch',
              handle: 'Billy Bob',
-             image_url: 'http://foo',
+             image_url: 'http://imakeityoumakeit.com/wp-content/uploads/2013/09/how-to-make-a-cat-bowtie-442x450.jpg',
              repo: 'prodigy',
              branch: 'master2',
              date: '2017-04-22',
