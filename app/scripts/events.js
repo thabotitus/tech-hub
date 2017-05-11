ba5d0a5285930de0912a58216978e3d2ccbc52dc
Patrick Vine <pvine@prodigyfinance.com>
2017-05-11 11:27:05 +0200

    jQuery with call to github working
    Fix up calling for aync style

--- app/scripts/events.js
+++ app/scripts/events.js
@@ -10,8 +10,47 @@ var events = new Vue({
   }
 })
 
-function fetch_from_github() {
-  return [ { type: 'push',
+
+
+
+
+
+
+
+
+
+
+
+// Functions for fetching data from github
+
+function fetch_data(auth_data) {
+  jQuery.ajax
+  ({
+    type: "GET",
+    url: "https://api.github.com/repos/x/y/events",
+    dataType: 'json',
+    headers: {"Authorization": "Basic " + btoa(auth_data)},
+    success: function (data){
+      alert(data[0]);
+      callback_method(the_list);
+    },
+    error: function() {
+      alert('error??');
+    }
+   });
+}
+
+function convert_events(events) {
+  // maps events to Vue...
+}
+
+function do_my_thing() {
+  fetch_from_github(convert_events, 'pv', 'xxxx');
+}
+
+function fetch_from_github(callback_method, user, token) {
+ // data = fetch_data(call_backmethod, user + ":" + token);
+  result = [ { type: 'push',
              handle: 'Billy Bob',
              image_url: 'https://s-media-cache-ak0.pinimg.com/736x/3e/de/0b/3ede0bf72b5e51dc895714f1f6d9ee7d.jpg',
              repo: 'prodigy',
@@ -28,4 +67,5 @@ function fetch_from_github() {
              description: 'Some cool stuff'
            },
   ];
+  callback_method(result);
 }
