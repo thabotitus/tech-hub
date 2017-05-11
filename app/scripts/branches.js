a08ddcad5bfb8cfc7898535e10aaa976878dee71
Patrick Vine <pvine@prodigyfinance.com>
2017-05-11 16:32:10 +0200

    create the branches call

--- app/scripts/branches.js
+++ app/scripts/branches.js
@@ -8,11 +8,34 @@ var branches = new Vue({
   },
   methods: {
     payload = [];
+
+    make_gh_call: function() {
+      vt = this;
+      auth_data = this.creds().username + ":" + this.creds().token;
+      jQuery.ajax
+      ({
+        type: "GET",
+        url: "https://api.github.com/repos/" + this.creds().org + "/"+ this.creds().repo +"/branches"
+        dataType: 'json',
+        headers: {"Authorization": "Basic " + btoa(auth_data)},
+        success: function (data){
+          vt.payload = vt.buildBranches(data, vt.payload);
+        },
+        error: function() {
+          alert('error');
+        }
+       });
+    },
+
+    creds: function() {
+      return github_details();
+    },
+
     buildBranches: function (data, payload) {
-      name = '',
-      isStale = '',
-      status = ''
       $.each(data, function (_, v) {
+        name = v.name;
+        isStale = '',
+        status = ''
         payload.push({
           name: name,
           isStale: isStale,
