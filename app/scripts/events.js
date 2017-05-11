c4a62bc64406b4bf244407c29d4bf1f92a13cc28
Patrick Vine <pvine@prodigyfinance.com>
2017-05-11 13:28:06 +0200

    remove overriding of callback method

--- app/scripts/events.js
+++ app/scripts/events.js
@@ -88,48 +88,84 @@ $(document).ready(function(){
 
 // Functions for fetching data from github
 
-// function fetch_data(callback_method, auth_data, org, repo) {
-//   jQuery.ajax
-//   ({
-//     type: "GET",
-//     url: "https://api.github.com/repos/"+org+"/"+repo+"/events",
-//     dataType: 'json',
-//     headers: {"Authorization": "Basic " + btoa(auth_data)},
-//     success: function (data){
-//       map_github_to_events_hash(callback_method, data);
-//       return call_me_back(data);
-//     },
-//     error: function() {
-//       alert('error??');
-//     }
-//    });
-// }
+function fetch_data(callback_method, auth_data, org, repo) {
+  jQuery.ajax
+  ({
+    type: "GET",
+    url: "https://api.github.com/repos/"+org+"/"+repo+"/events",
+    dataType: 'json',
+    headers: {"Authorization": "Basic " + btoa(auth_data)},
+    success: function (data){
+      map_github_to_events_hash(callback_method, data);
+    },
+    error: function() {
+      alert('error??');
+    }
+   });
+}
 
-// function map_github_to_events_hash(callback_method, data) {
-//   function event_from(event_type) {
-//     if(event_type == 'PushEvent') {
-//       return {type: 'push', description: 'Branch pushed'};
-//     } else if(event_type == 'CreateEvent') {
-//       return {type: 'create_branch', description: 'Branch created'};
-//     } else if(event_type == 'DeleteEvent') {
-//       return {type: 'delete_branch', description: 'Branch deleted'};
-//     }
-//     return {type: 'generic', description: 'Event of type '+ event_type +' occured'};
-//   }
-//   function branch_from(ref) {
-//     return ref.replace(/^refs\/heads\//, '');
-//   }
-//   event_hashes = $.map(data, function(event) {
-//     event_detail = event_from(event['type']);
-//     return {
-//       type: event_detail['type'],
-//       handle: event['actor']['display_login'],
-//       image_url: event['actor']['avatar_url'],
-//       repo: event['repo']['name'],
-//       branch: branch_from(event['payload']['ref']),
-//       date: event['created_at'],
-//       description: event_detail['description'] //'Branch pushed'
-//     };
-//   });
-//   callback_method(event_hashes);
-// }
+function map_github_to_events_hash(callback_method, data) {
+  function event_from(event_type) {
+    if(event_type == 'PushEvent') {
+      return {type: 'push', description: 'Branch pushed'};
+    } else if(event_type == 'CreateEvent') {
+      return {type: 'create_branch', description: 'Branch created'};
+    } else if(event_type == 'DeleteEvent') {
+      return {type: 'delete_branch', description: 'Branch deleted'};
+    }
+    return {type: 'generic', description: 'Event of type '+ event_type +' occured'};
+  }
+  function branch_from(ref) {
+    return ref.replace(/^refs\/heads\//, '');
+  }
+  event_hashes = $.map(data, function(event) {
+    event_detail = event_from(event['type']);
+    return {
+      type: event_detail['type'],
+      handle: event['actor']['display_login'],
+      image_url: event['actor']['avatar_url'],
+      repo: event['repo']['name'],
+      branch: branch_from(event['payload']['ref']),
+      date: event['created_at'],
+      description: event_detail['description'] //'Branch pushed'
+    };
+  });
+  callback_method(event_hashes);
+}
+
+
+function convert_events(events) {
+  // maps events to Vue...
+}
+
+function do_my_thing() {
+  fetch_from_github(convert_events);
+}
+
+function fetch_from_github(callback_method) {
+  github_creds = github_details();
+  fetch_data(call_backmethod,
+             github_creds['username'] + ":" + github_creds['token'],
+             github_creds['org'],
+             github_creds['repo']
+            );
+
+  result = [ { type: 'push',
+             handle: 'Billy Bob',
+             image_url: 'https://s-media-cache-ak0.pinimg.com/736x/3e/de/0b/3ede0bf72b5e51dc895714f1f6d9ee7d.jpg',
+             repo: 'prodigy',
+             branch: 'master',
+             date: '2017-03-22',
+             description: 'Some cool stuff'
+           },
+           { type: 'create_branch',
+             handle: 'Billy Bob',
+             image_url: 'http://imakeityoumakeit.com/wp-content/uploads/2013/09/how-to-make-a-cat-bowtie-442x450.jpg',
+             repo: 'prodigy',
+             branch: 'master2',
+             date: '2017-04-22',
+             description: 'Some cool stuff'
+           },
+  ];
+  callback_method(result);
+}
